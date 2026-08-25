3e8fac506dd83d0621ec5f606be24f3ba62d0b46
Miranto45 <comptemiranto784@gmail.com>
2026-08-26 02:43:44 +0300

    Ajout de la route clear-config avec import Artisan

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,13 @@
 
 use App\Http\Controllers\PortfolioController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::get('/', [PortfolioController::class, 'index'])->name('home');
 Route::get('/cv', [PortfolioController::class, 'cv'])->name('cv');
 Route::post('/contact', [PortfolioController::class, 'contact'])->name('contact');
+Route::get('/clear-config', function() {
+    Artisan::call('config:clear');
+    Artisan::call('cache:clear');
+    return 'Cache vidé avec succès !';
+});
